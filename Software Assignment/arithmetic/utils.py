8c39af69131232290d1f9b4b078636285a8d2558
Leo Benitah <l.f.benitah@student.tue.nl>
2025-09-13 14:03:48 +0200

    Add, subtract, pasrse, reverse parce, divide, modular multiply, and modular reduction done

--- Software Assignment/arithmetic/utils.py
+++ Software Assignment/arithmetic/utils.py
@@ -1,10 +1,123 @@
+def digitParse(num_str: str) -> int:
+    """
+    Converts a single-digit in base 16 to an hexadecimal integer.
+    """
+
+    if num_str == "0":
+        return 0
+    elif num_str == "1":
+        return 1
+    elif num_str == "2":
+        return 2
+    elif num_str == "3":
+        return 3
+    elif num_str == "4":
+        return 4
+    elif num_str == "5":
+        return 5
+    elif num_str == "6":
+        return 6
+    elif num_str == "7":
+        return 7
+    elif num_str == "8":
+        return 8
+    elif num_str == "9":
+        return 9
+    elif num_str == "A":
+        return 10
+    elif num_str == "B":
+        return 11
+    elif num_str == "C":
+        return 12
+    elif num_str == "D":
+        return 13
+    elif num_str == "E":
+        return 14
+    elif num_str == "F":
+        return 15
+    
+def reverseDigitParse(num_int: int) -> str:
+    """
+    Convert a single-digit hexadecimal integer to a hexadecimal string.
+    """
+    num_str = ""
+    if num_int == 0:
+        num_str = "0"
+    if num_int == 1:
+        num_str = "1"
+    if num_int == 2:
+        num_str = "2"
+    if num_int == 3:
+        num_str = "3"
+    if num_int == 4:
+        num_str = "4"
+    if num_int == 5:
+        num_str = "5"
+    if num_int == 6:
+        num_str = "6"
+    if num_int == 7:
+        num_str = "7"
+    if num_int == 8:
+        num_str = "8"
+    if num_int == 9:
+        num_str = "9"   
+    if num_int == 10:
+        num_str = "A"
+    elif num_int == 11:
+        num_str = "B"
+    elif num_int == 12:
+        num_str = "C"
+    elif num_int == 13:
+        num_str = "D"
+    elif num_int == 14:
+        num_str = "E"
+    elif num_int == 15:
+        num_str = "F"
+    return num_str
+    
 def parse(num_str: str, radix: int) -> int:
     """
-    Convert a single-digit or full number string in a given radix to an integer.
-    Works for radix 2-16.
+    Convert a single-digit or full number string in a given radix to a base 10 integer.
     """
-    return int(num_str, radix)
+    set_neg = False
+    if "-" in num_str:
+        num_str.replace("-", "")
+        set_neg = True
 
+    power = 0
+    total_sum = 0
+    for i in range(len(num_str)-1, -1, -1):
+        digit_val = digitParse(num_str[i])
+        digit_sum = digit_val * radix**power
+        power += 1
+        total_sum = total_sum + digit_sum
+    if set_neg == True:
+        return -total_sum
+    else:
+        return total_sum 
+
+def reverseParse(num_int: int, radix: int) -> str:
+    """
+    Convert a single-digit or full number base 10 integer into a string with a given radix.
+    """
+    set_neg = False
+    if num_int < 0:
+        num_int = abs(num_int)
+        set_neg = True
+    divided = num_int
+    if num_int == 0:
+        num_str = "0" 
+    else:    
+        num_str = ""
+    digit = ""
+    while divided != 0:
+        digit = reverseDigitParse(divided % radix)
+        num_str = digit + num_str
+        divided = (divided // radix)
+    if set_neg:
+            return "-" + num_str
+    else:
+        return num_str
 
 def zero_padding(x: str, y: str) -> tuple[str, str]:
     """
@@ -60,3 +173,5 @@ def compare_magnitude(x: str, y: str, radix: int) -> int:
         elif digit_x < digit_y:
             return -1
     return 0
+
+
