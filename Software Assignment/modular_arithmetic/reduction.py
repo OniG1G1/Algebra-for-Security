e5eb17dc34ac8d66e528a1a17463866bf1c77625
Leo Benitah <l.f.benitah@student.tue.nl>
2025-09-13 15:23:27 +0200

    Added Modular reduction, addition, subtraction and the framwork for integer division

---
+++ Software Assignment/modular_arithmetic/reduction.py
@@ -0,0 +1,14 @@
+from modulus.core import modularReduction
+
+class Reduction:
+    def execute(self, exercise: dict) -> dict:
+        """
+        Main entry point for reduction.
+        """
+        print(f"Executing operation: {type(self).__name__}")
+        x = exercise["x"]
+        y = exercise["y"]
+        radix = int(exercise["radix"])
+        m = exercise["modulus"]
+        result = modularReduction(x, y, radix, m)
+        return {"answer": result}
