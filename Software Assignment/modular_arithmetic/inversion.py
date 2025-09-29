7e90816e851eea68af27a7d8f26e9f4491bc5798
Olivier Wieczerynski <olivierws11@gmail.com>
2025-09-29 02:28:55 +0200

    Fixed modular inversion so that it works with negative x.

--- Software Assignment/modular_arithmetic/inversion.py
+++ Software Assignment/modular_arithmetic/inversion.py
@@ -12,11 +12,20 @@ def modular_inversion(exercise: dict) -> dict:
     x = exercise["x"]
     m = exercise["modulus"]
     radix = int(exercise["radix"])
+
+    sign_x = '-' if x.startswith('-') else '+'
+    x_val = x[1:] if sign_x == '-' else x
     
     # Inversion only exists if gcd(x,m) = 1
-    x1, y1, gcd = extendedEuclidean_raw(x, m, radix)
+    x1, y1, gcd = extendedEuclidean_raw(x_val, m, radix)
 
     if compare_numbers(m, "1", radix) == -1: answer = None
+
+    if sign_x == '-' and x1 != "0":
+        if not x1.startswith("-"):
+            x1 = "-" + x1
+        else:
+            x1 = x1[1:]
 
     if gcd != "1":
         answer = None
